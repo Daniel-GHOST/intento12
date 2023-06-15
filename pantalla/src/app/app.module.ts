f83898aabee7bba951a9cd07e4e52732fcb28f51
Daniel-GHOST <p0x01.ghost@gmail.com>
2023-06-14 20:43:27 -0600

    aqui te dejo el link de mi prompt con chatgpt

---
+++ pantalla/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PantallaPrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
