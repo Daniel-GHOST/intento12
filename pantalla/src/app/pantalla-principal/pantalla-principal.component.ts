e17eb6a9b85b0922ea4a421b4ad8937f252edb3f
Daniel-GHOST <p0x01.ghost@gmail.com>
2023-09-13 11:55:54 -0600

    mi primer commit

--- pantalla/src/app/pantalla-principal/pantalla-principal.component.ts
+++ pantalla/src/app/pantalla-principal/pantalla-principal.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./pantalla-principal.component.css']
 })
 export class PantallaPrincipalComponent {
+  meses: string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  ventasPorMes: number[] = [100, 150, 200, 180, 250, 300, 280, 350, 400, 380, 450, 500];
+  mostrarGrafica: boolean = false;
+  filtroSeleccionado: string = '';
 
+  toggleGrafica(): void {
+    this.mostrarGrafica = !this.mostrarGrafica;
+  }
+
+  filtrarVentas(filtro: string): void {
+    this.filtroSeleccionado = filtro;
+    // Aquí implementa la lógica para filtrar las ventas según el filtro seleccionado y actualizar la tabla y la gráfica
+  }
 }
